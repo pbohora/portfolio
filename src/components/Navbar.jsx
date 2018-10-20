7cda132c5769c1028f16db40bcb4a2d94cfd29dd
pbohora <pradeepbohora26@gmail.com>
2018-10-20 23:53:39 +0300

    navbar component created

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-expand-md bg-light navbar-light fixed-top">
+      <a className="navbar-brand">Pradip</a>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#collapsibleNavbar"
+      >
+        <span className="navbar-toggler-icon" />
+      </button>
+      <div className="collapse navbar-collapse" id="collapsibleNavbar">
+        <ul className="navbar-nav nav-tabs mr-auto">
+          <li className="nav-item">
+            <a className="nav-link" href="/">
+              Home
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="/about">
+              Porfolio
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="/contact">
+              Contact
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
