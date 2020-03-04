8b3a77ef1487243578bf90885e67ae30353a333d
pbohora <pradeepbohora26@gmail.com>
2020-03-04 14:53:24 +0200

    add new build

---
+++ client/build/precache-manifest.f8b2941f5a44a0daf8c561bf369ca17f.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "eb0412e2ca533294bec4",
+    "url": "/static/css/main.a2e038a4.chunk.css"
+  },
+  {
+    "revision": "eb0412e2ca533294bec4",
+    "url": "/static/js/main.eb0412e2.chunk.js"
+  },
+  {
+    "revision": "5ecdbac6e762136fb414",
+    "url": "/static/css/1.ff5e503d.chunk.css"
+  },
+  {
+    "revision": "5ecdbac6e762136fb414",
+    "url": "/static/js/1.5ecdbac6.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e847233c4c8ead8afd65a7308580f95e",
+    "url": "/static/media/profile.e847233c.jpg"
+  },
+  {
+    "revision": "ae95e662f03edc96dcacf673a469dfca",
+    "url": "/static/media/blog.ae95e662.png"
+  },
+  {
+    "revision": "3c950120a592bec6439c8ede70ef9c7d",
+    "url": "/static/media/phonebook.3c950120.jpg"
+  },
+  {
+    "revision": "20a2aadd16493b8038334cf178c501be",
+    "url": "/static/media/homess.20a2aadd.jpg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "2a3a3ade54c8fc4a9b4b8e243b6bcef0",
+    "url": "/index.html"
+  }
+];
