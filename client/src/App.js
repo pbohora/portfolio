ccefabc7242332828a8d34e93f37858f8e2e996f
pbohora <pradeepbohora26@gmail.com>
2020-03-14 20:38:07 +0200

    remove contact to fix bug

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,24 @@
-import React, { Component } from "react";
-import Home from "./components/Home";
-import About from "./components/About";
-import Portfolio from "./components/Portfolio";
-import Contact from "./components/Contact";
-import Footer from "./components/Footer";
-import Navbar from "./components/Navbar";
+import React, { Component } from 'react'
+import Home from './components/Home'
+import About from './components/About'
+import Portfolio from './components/Portfolio'
+import Contact from './components/Contact'
+import Footer from './components/Footer'
+import Navbar from './components/Navbar'
 
-import { Provider as AlertProvider } from "react-alert";
-import AlertTemplate from "react-alert-template-basic";
-import { BrowserRouter } from "react-router-dom";
+import { Provider as AlertProvider } from 'react-alert'
+import AlertTemplate from 'react-alert-template-basic'
+import { BrowserRouter } from 'react-router-dom'
 
-import "./app.css";
+import './app.css'
 const options = {
   timeout: 5000,
-  position: "top center",
+  position: 'top center',
   zIndex: 2500,
-  offset: "100px",
-  transition: "scale",
-  type: "success"
-};
+  offset: '100px',
+  transition: 'scale',
+  type: 'success'
+}
 class App extends Component {
   render() {
     return (
@@ -29,13 +29,13 @@ class App extends Component {
           <About />
           <Portfolio />
           <AlertProvider template={AlertTemplate} {...options}>
-            <Contact />
+            {/* <Contact /> */}
           </AlertProvider>
           <Footer />
         </div>
       </BrowserRouter>
-    );
+    )
   }
 }
 
-export default App;
+export default App
--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -1,138 +1,137 @@
-import React, { Component } from "react";
-import { withAlert } from "react-alert";
-import "../contact.css";
-
+import React, { Component } from 'react'
+import { withAlert } from 'react-alert'
+import '../contact.css'
 class Contact extends Component {
   state = {
-    name: "",
-    nameError: "",
-    email: "",
-    emailError: "",
-    message: "",
-    messageError: ""
-  };
+    name: '',
+    nameError: '',
+    email: '',
+    emailError: '',
+    message: '',
+    messageError: ''
+  }
 
   handleChange = e => {
     this.setState({
       [e.target.id]: e.target.value
-    });
-  };
+    })
+  }
 
   validate = () => {
-    let isError = false;
+    let isError = false
     const errors = {
-      nameError: "",
-      emailError: "",
-      messageError: ""
-    };
-
-    if (this.state.name.length < 3) {
-      isError = true;
-      errors.nameError = "Username needs to be atleast 3 characters long!";
+      nameError: '',
+      emailError: '',
+      messageError: ''
     }
 
-    if (this.state.email.indexOf("@") === -1) {
-      isError = true;
-      errors.emailError = "Requires valid email!";
+    if (this.state.name.length < 3) {
+      isError = true
+      errors.nameError = 'Username needs to be atleast 3 characters long!'
+    }
+
+    if (this.state.email.indexOf('@') === -1) {
+      isError = true
+      errors.emailError = 'Requires valid email!'
     }
 
     if (this.state.message.length < 5) {
-      isError = true;
-      errors.messageError = "Please write a valid message!";
+      isError = true
+      errors.messageError = 'Please write a valid message!'
     }
 
     this.setState({
       ...this.state,
       ...errors
-    });
+    })
 
-    return isError;
-  };
+    return isError
+  }
 
   handleSubmit = e => {
-    e.preventDefault();
-    const err = this.validate();
+    e.preventDefault()
+    const err = this.validate()
 
     if (!err) {
-      fetch("/api/world", {
-        method: "POST",
+      fetch('/api/world', {
+        method: 'POST',
         body: JSON.stringify({ message: this.state.message }),
         headers: {
-          "Content-Type": "application/json"
+          'Content-Type': 'application/json'
         }
       })
         .then(
           this.props.alert.show(
-            <div className="text-center">
+            <div className='text-center'>
               <h6>Thank you for your message !</h6>
               <p>I will get back to you soon</p>
             </div>
           )
         )
-        .then(this.setState({ name: "", email: "", message: "" }));
+        .then(this.setState({ name: '', email: '', message: '' }))
     }
-  };
+  }
   render() {
     return (
-      <div className="background" id="contact">
-        <div className="container pb-4">
-          <div className="row">
-            <div className="col-lg text-center m-5">
-              <h1 className="text-muted">Contact</h1>
+      <div className='background' id='contact'>
+        <div className='container pb-4'>
+          <div className='row'>
+            <div className='col-lg text-center m-5'>
+              <h1 className='text-muted'>Contact</h1>
             </div>
           </div>
 
           <form
             onSubmit={this.handleSubmit}
-            className="form-styles mb-4 bg-white"
+            className='form-styles mb-4 bg-white'
           >
-            <div className="form-row">
-              <div className="form-group col-lg-6">
-                <label htmlFor="">Full Name</label>
+            <div className='form-row'>
+              <div className='form-group col-lg-6'>
+                <label htmlFor=''>Full Name</label>
                 <input
-                  type="text"
-                  id="name"
+                  type='text'
+                  id='name'
                   onChange={this.handleChange}
                   value={this.state.name}
-                  className="form-control"
-                  placeholder="Enter your Full Name"
+                  className='form-control'
+                  placeholder='Enter your Full Name'
                 />
-                <p className="text-danger">{this.state.nameError}</p>
+                <p className='text-danger'>{this.state.nameError}</p>
               </div>
 
-              <div className="form-group col-lg-6">
-                <label htmlFor="">Email Address</label>
+              <div className='form-group col-lg-6'>
+                <label htmlFor=''>Email Address</label>
                 <input
-                  type="text"
-                  id="email"
+                  type='text'
+                  id='email'
                   onChange={this.handleChange}
                   value={this.state.email}
-                  className="form-control "
-                  placeholder="Enter Email"
+                  className='form-control '
+                  placeholder='Enter Email'
                 />
-                <p className="text-danger">{this.state.emailError}</p>
+                <p className='text-danger'>{this.state.emailError}</p>
               </div>
             </div>
 
-            <div className="form-group">
-              <label htmlFor="">Message</label>
+            <div className='form-group'>
+              <label htmlFor=''>Message</label>
               <textarea
-                type="text"
-                id="message"
+                type='text'
+                id='message'
                 onChange={this.handleChange}
                 value={this.state.message}
-                className="form-control "
-                row="15"
+                className='form-control '
+                row='15'
               />
-              <p className="text-danger">{this.state.messageError}</p>
+              <p className='text-danger'>{this.state.messageError}</p>
             </div>
 
-            <button className="btn btn-primary button">Send Message</button>
+            <button className='btn btn-primary button'>Send Message</button>
           </form>
         </div>
       </div>
-    );
+    )
   }
 }
 
-export default withAlert(Contact);
+export default withAlert(Contact)
